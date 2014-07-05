7ccb4271e4550b4d564e0f6ce888cc9d5f4e88d1
Tait Larson <telarson@gmail.com>
2014-07-04 21:28:52 -0400

    got histogram working.

--- hw3/Golf.hs
+++ hw3/Golf.hs
@@ -1,16 +1,15 @@
 
 module Golf where
 
+import Data.List
+
 -- generate "nth" indexes
 skipsNth :: Int -> Int -> [Int]
 skipsNth len 0 = [0..(len-1)]
 skipsNth len n = foldr (\i acc -> if i `rem` n == 0 then (i-1):acc  else acc) []  [1..len]
--- skipsNth len n = filter (flip (rem) n) [1..len]
 
 -- generate indexes
 skipsGI :: [a] -> [[Int]]
---skipsGI "ABCD" = [ [0,1,2,3], [1,3], [2], [3] ]
--- get the length generate numbers up to length, "length" times. you would now have a list to work with...
 skipsGI a = let la = length a in map (skipsNth la) [1..la]
 
 -- get one element of the result
@@ -32,3 +31,30 @@ genTriples xs = take 3 xs : genTriples (tail xs)
 localMaxima :: [Int] -> [Int]
 -- generate all lists of 3 items or more
 localMaxima is = foldl (\acc (a:b:c:_) -> if midLarge a b c then b:acc else acc ) [] $ genTriples is
+
+data HistLine = HistLine HistDots HistMore
+type HistMore = [Int]
+type HistDots = [Int]
+
+-- TODO: can we use a data type for the return type?  
+-- hdistinct :: [Int] -> [[Int]]
+hdistinct :: [Int] -> HistLine
+hdistinct is = let u = nub is in HistLine u (is \\ u) 
+
+hprintLine :: HistDots -> String
+hprintLine is = foldr (\x acc -> if x `elem` is then '*':acc else ' ':acc) [] [1..9]
+
+hkey :: String
+hkey = replicate 9 '=' ++ "\n123456789\n" 
+
+histogram :: [Int] -> String
+histogram is = histogram' is ++ "\n" ++ hkey
+
+-- print as you go. remove items from the list. stop printing new lines once the list is finished. 
+-- 
+histogram' :: [Int] -> String
+histogram' is = let HistLine dots more = hdistinct is in 
+                   case more of 
+                      [] -> hprintLine dots
+                      _  -> histogram' more ++ "\n" ++ hprintLine dots
+
