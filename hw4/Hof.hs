d3e097db5d2eddd6b6acd77de40df19489e30bdc
Tait Larson <telarson@gmail.com>
2014-07-05 13:19:19 -0400

    added quick comment.

--- hw4/Hof.hs
+++ hw4/Hof.hs
@@ -8,5 +8,6 @@ fun2 :: Integer -> Integer
 fun2 1 = 0
 fun2 x = fun2' x
 
+-- this is wrong.  div 2 can return an odd. 
 fun2' :: Integer -> Integer
 fun2' =  sum . takeWhile (>1) . iterate (`div` 2) . (\n -> if odd n then 3 * n + 1 else n)
