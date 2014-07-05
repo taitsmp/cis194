35170afe6d3bfc0fed3e8d0707769bb1db1a212a
Tait Larson <telarson@gmail.com>
2014-07-05 13:14:19 -0400

    first attempt at fun1 and fun2

--- hw4/Hof.hs
+++ hw4/Hof.hs
@@ -3,5 +3,10 @@ module Hof where
 fun1 :: [Integer] -> Integer
 fun1 = product . map (subtract 2) . filter even
 
--- fun2 :: [Integer] -> Integer
--- fun2 = 
+-- collatz-like
+fun2 :: Integer -> Integer
+fun2 1 = 0
+fun2 x = fun2' x
+
+fun2' :: Integer -> Integer
+fun2' =  sum . takeWhile (>1) . iterate (`div` 2) . (\n -> if odd n then 3 * n + 1 else n)
