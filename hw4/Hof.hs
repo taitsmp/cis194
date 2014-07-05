e5661f3e97b43ac843fd5f3292c714080c49b10e
Tait Larson <telarson@gmail.com>
2014-07-05 14:20:13 -0400

    possible fix for fun2

--- hw4/Hof.hs
+++ hw4/Hof.hs
@@ -5,9 +5,4 @@ fun1 = product . map (subtract 2) . filter even
 
 -- collatz-like
 fun2 :: Integer -> Integer
-fun2 1 = 0
-fun2 x = fun2' x
-
--- this is wrong.  div 2 can return an odd. 
-fun2' :: Integer -> Integer
-fun2' =  sum . takeWhile (>1) . iterate (`div` 2) . (\n -> if odd n then 3 * n + 1 else n)
+fun2 =  sum . filter even . takeWhile (>1) . iterate (\n -> if odd n then 3 * n + 1 else n `div` 2)
