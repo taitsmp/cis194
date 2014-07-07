0ee69e9457ab5d92717ed8166ce6d8abbfcd4b6e
Tait Larson <telarson@gmail.com>
2014-07-07 09:01:18 -0400

    finished hw4. Need some testing.

--- hw4/Hof.hs
+++ hw4/Hof.hs
@@ -1,5 +1,7 @@
 module Hof where
 
+import Data.List
+
 fun1 :: [Integer] -> Integer
 fun1 = product . map (subtract 2) . filter even
 
@@ -15,42 +17,51 @@ data Tree a = Leaf
 -- keep tree balanced.  Always insert node into shortest path. 
 -- accumulator keeps track of new tree state. 
 -- use foldr so the last element is inserted first. 
--- need helper function to insert node into tree?  just use it when need to return accumulator? given node and tree return a new tree? could just fold on that fn?
--- how to do breadth first search for "leaf" for insert? Ensure only one leaf found and one insert made.
--- pass calculated distance from root to leaf via recursion. leaf with smallest dist is where we'd want to insert. 
--- update all heights after insert(?) update height as you follow path? 
 foldTree :: [a] -> Tree a
-foldTree xs = foldr (treeInsert) Leaf xs
+foldTree xs = let len = length xs in 
+                           foldr (treeInsert) Leaf $ zipWith (\h a -> (h,a)) (heights len) xs
 
-treeInsert :: a -> Tree a -> Tree a
-treeInsert x Leaf = (Node 0 Leaf x Leaf)
-treeInsert x (Node ht t1 y t2) 
-   | d1 < d2 = (Node ht (treeInsert x t1) y t2) -- wish I could just update the height here but I can't...would be wrong and vars are immutable.  
-   | otherwise = (Node ht t1 y (treeInsert x t2))
+
+treeInsert :: (Integer, a) -> Tree a -> Tree a
+treeInsert (h,x) Leaf = (Node h Leaf x Leaf)
+treeInsert tup (Node ht t1 y t2) 
+   | d1 < d2 = (Node ht (treeInsert tup t1) y t2) 
+   | otherwise = (Node ht t1 y (treeInsert tup t2))
    where d1 = depthOfClosestLeaf 0 t1
          d2 = depthOfClosestLeaf 0 t2
 
+-- if this function is wrong replace it with a function that counts leaves in a tree.  subtree with fewest leaves is one way to determine where to insert (after comparing subtree heights). 
 depthOfClosestLeaf :: Int -> Tree a -> Int 
 depthOfClosestLeaf i Leaf = i
-depthOfClosestLeaf i (Node h t1 _ t2) = min d1 d2 
+depthOfClosestLeaf i (Node _ t1 _ t2) = min d1 d2 
                                           where d1 = depthOfClosestLeaf (i+1) t1 
                                                 d2 = depthOfClosestLeaf (i+1) t2
 
--- start at leafs.  check height of left and right subtree.  height of current node is 1+max height. 
--- need to pass around the height? 
-UpdateHeight :: Tree a -> Tree a
-UpdateHeight Leaf = Leaf
-UpdateHeight (Node h Leaf a t) case t of
-                                 (Node ht _ _ _) -> (Node ht+1 Leaf a t)
-                                 Leaf -> (Node 0 Leaf a Leaf)
-UpdateHeight (Node h t1 a t2) -> 
-              
 
---treeHeight :: Tree a -> Int
---treeHeight Leaf = 0
---treeHeight (Node 
+-- idea: you know the number of items to insert.  calculate height of each item before you insert it. 
+heights :: Int -> [Integer]
+heights i = map toInteger . take i $ heights' i i 
 
--- when to call this?  height will be inaccurate right after an insert.  
--- treeHeight :: Tree a -> Int
--- treeHeight Leaf = 0
--- treeHeight (Node i t1 a t2) = (max h1 h2) + 1 where
+heights' :: Int -> Int -> [Int] -- start and num items in list?
+heights' start 0 = let nodes = start ^ 2 in replicate nodes 0
+heights' start dep = let nodes = (2^(start - dep)) in (replicate nodes dep) ++ (heights' start (dep-1)) 
+
+-- implement xor as a fold
+xor :: [Bool] -> Bool
+xor = foldr (\x acc -> if x then not acc else acc) False 
+
+-- implement map as a fold
+map' :: (a -> b) -> [a] -> [b]
+map' f = foldr (\x acc -> (f x):acc) [] 
+
+-- given n generate all primes up to 2n + 2
+sieveSundaram :: Integer -> [Integer]
+sieveSundaram n = let m = 2*n+2 in takeWhile (<m) . map (\k -> 2*k+1) $ step1 n
+
+-- generate the numbers to remove from the list first. 
+step1 :: Integer -> [Integer]
+step1 n = [1..n] \\ (map (\(a,b) -> a+b+2*a*b ) $ cartProd [1..n] [1..n])
+
+cartProd :: [a] -> [b] -> [(a, b)]
+cartProd xs ys = [(x,y) | x <- xs, y <- ys] 
+
