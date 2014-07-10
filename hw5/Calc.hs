e1906ccf26c766fc15873d1622df3d3c2ce2ec41
Tait Larson <telarson@gmail.com>
2014-07-10 08:14:51 -0400

    exercise 2 done on hw5.

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -2,8 +2,15 @@
 module Calc where
 
 import ExprT 
+import Parser
 
 eval :: ExprT -> Integer
 eval (Lit i) = i
 eval (Add e1 e2) =  (eval e1) + (eval e2)
 eval (Mul e1 e2) =  (eval e1) * (eval e2)
+
+evalStr :: String -> Maybe Integer
+evalStr s = case parseExp Lit Add Mul s of
+              Nothing -> Nothing
+              Just ae -> Just (eval ae) 
+
