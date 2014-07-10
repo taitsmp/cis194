dd8cfe26d0ac93277a4e515579a6f7f8483c398c
Tait Larson <telarson@gmail.com>
2014-07-09 22:12:12 -0400

    exercise 1 is done.

--- hw5/Calc.hs
+++ hw5/Calc.hs
@@ -3,5 +3,7 @@ module Calc where
 
 import ExprT 
 
-eval :: ExprT -> Int
-eval _ = 1
+eval :: ExprT -> Integer
+eval (Lit i) = i
+eval (Add e1 e2) =  (eval e1) + (eval e2)
+eval (Mul e1 e2) =  (eval e1) * (eval e2)
