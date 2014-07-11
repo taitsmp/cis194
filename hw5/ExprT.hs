c3c4f7cd36385e7b19aff7597b0bfb2763e3c62e
Tait Larson <telarson@gmail.com>
2014-07-11 08:35:42 -0400

    working on hw5 exercise 4.

--- hw5/ExprT.hs
+++ hw5/ExprT.hs
@@ -13,3 +13,27 @@ instance Expr ExprT where
   lit i = Lit i
   mul i j = Mul i j
   add i j = Add i j 
+
+instance Expr Integer where
+  lit i = i
+  mul i j = i * j
+  add i j = i + j
+
+instance Expr Bool where
+  lit i = i > 0 
+  mul i j = i &&  j
+  add i j = i || j
+
+newtype MinMax = MinMax Integer deriving (Eq, Show, Ord)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit = MinMax 
+  mul = min
+  add = max
+ 
+-- not working. type errors 
+instance Expr Mod7 where
+  lit  = Mod7
+  add i j = (i + j) `mod` 7  
+  mul i j = (i * j) `mod` 7
