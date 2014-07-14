dedf260cc3bcb20855025b42c045523805ace64f
Tait Larson <telarson@gmail.com>
2014-07-14 08:17:27 -0400

    close to getting 5.5 done.

--- hw5/Calculator.hs
+++ hw5/Calculator.hs
@@ -5,9 +5,11 @@ import StackVM
 
 -- exercise 5
 -- create an instance of the Expr type class for Program - expressions interpretted as compiled programs
+-- like walking an abstract syntax tree.  trying to build up the program/stack. 
+-- TODO: what to do with Bools? Am I missing something? Change def of Expr?
 instance Expr Program where
-  lit i = i > 0 
-  mul i j = i &&  j
-  add i j = i || j
+  lit i = [PushI i] 
+  mul i j = i ++ j ++ [StackVM.Mul]
+  add i j = i ++ j ++ [StackVM.Add]
 
 
