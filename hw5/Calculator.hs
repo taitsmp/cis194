dc9ac8f5a56fdc55ece487aaf456ffd96e6c4014
Tait Larson <telarson@gmail.com>
2014-07-15 07:05:46 -0400

    hw5 done.

--- hw5/Calculator.hs
+++ hw5/Calculator.hs
@@ -2,14 +2,7 @@
 
 import ExprT
 import StackVM
+import Parser
 
--- exercise 5
--- create an instance of the Expr type class for Program - expressions interpretted as compiled programs
--- like walking an abstract syntax tree.  trying to build up the program/stack. 
--- TODO: what to do with Bools? Am I missing something? Change def of Expr?
-instance Expr Program where
-  lit i = [PushI i] 
-  mul i j = i ++ j ++ [StackVM.Mul]
-  add i j = i ++ j ++ [StackVM.Add]
-
-
+compile :: String -> Maybe StackVM.Program
+compile s = parseExp lit add mul s
--- hw5/ExprT.hs
+++ hw5/ExprT.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, TypeSynonymInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, TypeSynonymInstances, FlexibleInstances #-}
 module ExprT where
 
 import Prelude
+import qualified StackVM as SVM
 
 class Expr a where
    lit :: Integer -> a
@@ -36,10 +37,17 @@ instance Expr MinMax where
   mul = min
   add = max
  
--- not working. type errors 
 instance Expr Mod7 where
   lit  = Mod7
   add i j = (i + j) `mod` 7 
   mul i j = (i * j) `mod` 7
 
+-- exercise 5
+-- create an instance of the Expr type class for Program - expressions interpretted as compiled programs
+-- like walking an abstract syntax tree.  trying to build up the program/stack. 
+-- TODO: what to do with Bools? Am I missing something? Change def of Expr?
+instance Expr SVM.Program where
+  lit i = [SVM.PushI i] 
+  mul i j = i ++ j ++ [SVM.Mul]
+  add i j = i ++ j ++ [SVM.Add]
 
