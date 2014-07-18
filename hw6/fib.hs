2eb2deff0cde83a1d8272d52d418d9f1cf118b6c
Tait Larson <telarson@gmail.com>
2014-07-18 09:01:10 -0400

    hw1 part1 is done.

--- hw6/fib.hs
+++ hw6/fib.hs
@@ -6,8 +6,8 @@ fib 0 = 0
 fib 1 = 1
 fib n = fib (n-1) + fib (n-2)
 
-fibs1 :: [Integer]
-fibs1 = iterate (\n -> fib (n+1)) 0 -- can we use iterate for fibs1?  Probably not.  Will just return zeros. 
+fibs1' :: [Integer]
+fibs1' = map fib [1..]
 
 
 
