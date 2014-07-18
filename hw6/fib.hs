fce81d38a1907c6f855f8721ac24b7ef6678b85d
Tait Larson <telarson@gmail.com>
2014-07-18 06:09:43 -0400

    working on hw6

--- hw6/fib.hs
+++ hw6/fib.hs
@@ -4,6 +4,10 @@ module Fib where
 fib :: Integer -> Integer
 fib 0 = 0
 fib 1 = 1
-fib n = (fib n-1) + (fib n-2)
+fib n = fib (n-1) + fib (n-2)
+
+fibs1 :: [Integer]
+fibs1 = iterate (\n -> fib (n+1)) 0 -- can we use iterate for fibs1?  Probably not.  Will just return zeros. 
+
 
 
