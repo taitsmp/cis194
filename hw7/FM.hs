df067db5e79f180e68599e936fe4609085e08383
Tait Larson <telarson@gmail.com>
2014-08-05 22:11:12 -0400

    practice

--- hw7/FM.hs
+++ hw7/FM.hs
@@ -1,6 +1,8 @@
 
 module FM where
 
+import Data.Monoid
+
 data Tree a = Empty
             	| Node (Tree a) a (Tree a)
   deriving(Show, Eq)
@@ -8,3 +10,26 @@ data Tree a = Empty
 treeFold :: b -> (b -> a -> b -> b) -> Tree a -> b
 treeFold e _ Empty        = e
 treeFold e f (Node l x r) = f (treeFold e f l) x (treeFold e f r)
+
+data ExprT = Lit Integer
+           | Add ExprT ExprT
+           | Mul ExprT ExprT
+
+eval :: ExprT -> Integer
+eval (Lit i)     = i
+eval (Add e1 e2) = eval e1 + eval e2
+eval (Mul e1 e2) = eval e1 * eval e2
+
+-- e1, e2 and i act like accumulators
+-- you need one higher level argument for each of the data types constructors. f => Lit i, g => Add, h => Mul
+exprTFold :: (Integer -> b) -> (b -> b -> b) -> (b -> b -> b) -> ExprT -> b
+exprTFold f _ _ (Lit i)     = f i
+exprTFold f g h (Add e1 e2) = g (exprTFold f g h e1) (exprTFold f g h e2)
+exprTFold f g h (Mul e1 e2) = h (exprTFold f g h e1) (exprTFold f g h e2)
+
+eval2 :: ExprT -> Integer
+eval2 = exprTFold id (+) (*)
+
+
+
+
