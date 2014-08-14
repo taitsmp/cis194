c357babd43c4adc1fcf5b2e8b139769520bcb32c
Tait Larson <telarson@gmail.com>
2014-08-14 07:30:34 -0400

    tested indexJ and it seems to work.  Could test it more on empty elements in the tree.

--- hw7/JoinList.hs
+++ hw7/JoinList.hs
@@ -21,9 +21,6 @@ tagi j = getSize . size $ tag j
 (+++) :: Monoid m => JoinList m a -> JoinList m a -> JoinList m a
 (+++) a b = Append (tag a `mappend` tag b) a b
 
--- helper function.  given the moniod 
-
-
 -- return element 'a' at the provided index from the joinlist b a.
 indexJ :: (Sized b, Monoid b) => 
           Int -> JoinList b a -> Maybe a
@@ -34,12 +31,14 @@ indexJ i (Single _ x)
 indexJ i (Append m j1 j2) 
      | i > last' = Nothing
      | i < 0     = Nothing 
-     | otherwise = let j = getSize . size $ tag j1
-                       k = getSize . size $ tag j2
+     | otherwise = let j = (getSize . size $ tag j1)  
                    in if i < j then indexJ i j1 else indexJ (i-j) j2
-                                                             
      where 
        last' = getSize(size m) - 1 
+
+-- tested and working on inputs like
+-- indexJ 2  (Append (Size 3) (Single (Size 1) 200) (Append (Size 2) (Single (Size 1) 44) (Single (Size 1) 10)))
+-- indexJ 0  (Append (Size 2) (Single (Size 1) 200) (Single (Size 1) 10))
 
 -- another way to do indexJ
 -- 1. use +++ to create a JoinList of only the nodes on the path to the element we want (always use right child?). track the number of moves to get to the element.  this is i.
@@ -51,11 +50,14 @@ dropJ :: (Sized b, Monoid b) =>
          Int -> JoinList b a -> JoinList b a
 dropJ _ Empty = Empty
 dropJ i (Single m n) 
-      | i <=0 = (Single m n)
+      | i == 0 = (Single m n) 
       | otherwise = Empty
 dropJ i (Append _ j1 j2) = let p = (tagi j1) - i
                                q = i - (tagi j2) 
                            in (dropJ p j1) +++ (dropJ q j2)
 
+-- is takeJ just the opposite of dropJ
+--takeJ :: (Sized b, Monoid b) =>
+--         Int -> JoinList b a -> JoinList b a
 
 
