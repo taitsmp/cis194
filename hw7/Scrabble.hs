959e0dc9f0b46993bf95bffb1db2ff7afc5d21b2
Tait Larson <telarson@gmail.com>
2014-08-19 08:28:25 -0400

    added new Scrabble Monoid

---
+++ hw7/Scrabble.hs
@@ -0,0 +1,24 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving, FlexibleInstances #-}
+module Scrabble where
+
+import Data.Monoid
+import Data.List
+import Data.Char
+import Data.Maybe
+
+newtype Score = Score Int deriving (Eq, Ord, Show, Num)
+
+instance Monoid Score where
+  mempty  = Score 0
+  mappend = (+) 
+
+score' :: Char -> Int
+score' c = fromMaybe 0 $ findIndex (\a -> toLower c `elem` a) [[], "aeilnorst", "dg", "bcmp", "fhvwy", "k", [], [], "j", [], "qz"]
+
+score :: Char -> Score
+score c = Score (score' c)
+           
+--scoreString :: String -> Score
+-- could I calculate this with a Monoid? 
+
+
