db0bf6fe6f6bea14e149d40f9b3c2eea4f7a3172
Tait Larson <tait@stylemepretty.com>
2014-08-21 21:55:31 -0400

    working on implementing JoinListBuffer for ex 4

--- hw7/JoinListBuffer.hs
+++ hw7/JoinListBuffer.hs
@@ -9,16 +9,26 @@ instance Buffer (JoinList(Score, Size) String) where
 
   -- | Convert a buffer to a String.
   toString :: JoinList(Score, Size) String -> String
-  toString Empty          = []
-  toString Single _ s     = s
-  toString Append _ j1 j2 = [] -- not finished 
+  toString j = joinListFold [] (++) id  
 
   -- | Create a buffer from a String.
   fromString :: String -> JoinList(Score, Size) String 
+  fromStrng s = let sc = scoreString s
+                    sz = length s 
+                in Single (sc, Size sz) s
 
   -- | Extract the nth line (0-indexed) from a buffer.  Return Nothing
   -- for out-of-bounds indices.
   line :: Int -> b -> Maybe String
 
---lots more to do here. 
- 
+   -- | @replaceLine n ln buf@ returns a modified version of @buf@,
+  --   with the @n@th line replaced by @ln@.  If the index is
+  --   out-of-bounds, the buffer should be returned unmodified.
+  replaceLine :: Int -> String -> b -> b
+
+  -- | Compute the number of lines in the buffer.
+  numLines :: b -> Int
+
+  -- | Compute the value of the buffer, i.e. the amount someone would
+  --   be paid for publishing the contents of the buffer.
+  value :: b -> Int
